8cd38aac7bb3a95c65026548b8e8017b4c771bae
qspidersseleniumoar <qspidersseleniumoar1234@gmail.com>
2021-08-30 16:48:00 +0530

    puch to github

---
+++ SeleniumFramework/src/test/java/pac1/CreateOrgTest.java
@@ -0,0 +1,25 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrg Test");
+ 	}	
+	
+	
+	@Test
+	public void createOrgWithIndustriesTest() {
+		System.out.println("execute createOrgWithIndustries Test");
+ 	}
+	
+	
+	@Test
+	public void createOrgTestWithRatingTest() {
+		System.out.println("execute createOrgTestWithRating Test");
+ 	}
+
+}
